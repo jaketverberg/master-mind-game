77933e6080400fad0ddbe0cfcefa31947e8b193c
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2022-01-04 13:30:38 +0100

    Updated some functions

--- game.rb
+++ game.rb
@@ -1,6 +1,6 @@
 module MasterMind
-  COLOR_CHOICES = [["Red"], ["Green"], ["Blue"], ["Magenta"], ["Cyan"], ["White"]]
-  TURNS = 12
+  COLOR_CHOICES = [["red"], ["green"], ["blue"], ["magenta"], ["cyan"], ["white"]]
+  turns = 12
 
   class String
     def bg_red;         "\e[41m#{self}\e[0m" end
@@ -12,6 +12,8 @@ module MasterMind
   end
 
   class Game
+    attr_reader :player_guess
+
     def initialize(player)
       temp_color_choice = []
       temp_color_choice[0] = "  ".bg_red
@@ -29,16 +31,24 @@ module MasterMind
     def play
       loop do
         #player selects their choices
-        player_guess_sequence
-        #if choices match computer_Cipher_sequence
-          #they win!
+        @player_guess = []
+        player_guess_sequence()
+
         if player_has_won?
           puts "#{player.name} won! Way to go!"
-          puts computer_cipher_sequence
+          puts @player_guess
           return
-        #if choices don't match computer_cipher_sequence turn - 1
+        elsif
+          turn == 0
+          "Out of turns! You lost."
+          return
+        else
+          turns - 1
+          puts "#{turns} turn(s) left"
+        end
 
-        #if turn == 0, game over, player looses
+        feedback()
+
 
         #for each color in computer_cipher_sequence
           #if color matches computer_cipher_sequence.color
@@ -50,30 +60,53 @@ module MasterMind
 
     end #end play
 
-    def player_has_won?
-      computer_cipher_sequence == player_guess ? true : false
-    end #end player_has_won?
-
-    def player_guess_sequence
-      puts "#{player.name} pick 4 choices by typing from the selections below."
-      COLOR_CHOICES.each { |color| puts color }
-      puts "** type your choices like so - 'Red' **"
-
-      player_guesses = []
-      4.times { |i| puts "Guess # #{i}" player_guess.push(gets.chomp.downcase) }
-      player_guess_color_conversion(player_guesses)
-
-    end #player_guess_sequence
-
-    def player_guess_color_conversion(guesses)
-
-    end #end player_guess_color_conversion
-
-    def feedback
-
+    def feedback(@player_guess)
+      #for each color in computer_cipher_sequence
+          #if color matches computer_cipher_sequence.color
+            #feedback + red
+          #else if color matches.any computer_cipher_sequence BUT NOT INDEX
+            #feedback + white
+          #else if color doesn't match.any? or index or computer_cipher_sequence
+            #feedback + blank
     end #end feedback
 
-  end
+    def player_has_won?
+      computer_cipher_sequence == @player_guess ? true : false
+    end #end player_has_won?
+
+    def player_guess_sequence()
+      puts "#{player.name} pick 4 choices one at a time by typing from the selections below."
+      COLOR_CHOICES.each { |color| puts color }
+      puts "** type your choice like so - 'Red' **"
+
+      loop do
+        guess = gets.chomp.downcase
+
+        COLOR_CHOICES.any? == guess ? @player_guess.push(guess) : puts "Not one of the choices, try again"
+        if @player_guess.length == 4
+          puts "Your guesses #{@player_guess.join('')}"
+          return
+        end
+      end #end loop
+
+      player_guess_text_to_color(@player_guess)
+    end #player_guess_sequence
+
+    def player_guess_text_to_color(guesses)
+      guesses.map! do |text_color|
+        case text_color
+        when "red"
+          text_color = "  ".bg_red
+        when "green"
+          text_color = "  ".bg_green
+        when "blue" = "  ".bg_blue
+        when "magenta" = "  ".bg_magenta
+        when "cyan" = "  ".bg_cyan
+        when "white" = "  ".bg_white
+        end #end case
+    end #end player_guess_text_to_color
+
+  end #Game Class
 
   class Player
     attr_reader :name
