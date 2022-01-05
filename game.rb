ab971be6b5ea41e4bce41f276303a5a0202bcda2
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2022-01-05 09:18:44 +0100

    Updated formatting

--- game.rb
+++ game.rb
@@ -1,78 +1,94 @@
 module MasterMind
-  COLOR_CHOICES = [["red"], ["green"], ["blue"], ["magenta"], ["cyan"], ["white"]]
+  COLOR_CHOICES = [['red'], ['green'], ['blue'], ['magenta'], ['cyan'], ['white']]
   turns = 12
 
   class String
-    def bg_red;         "\e[41m#{self}\e[0m" end
-    def bg_green;       "\e[42m#{self}\e[0m" end
-    def bg_blue;        "\e[44m#{self}\e[0m" end
-    def bg_magenta;     "\e[45m#{self}\e[0m" end
-    def bg_cyan;        "\e[46m#{self}\e[0m" end
-    def bg_white;       "\e[47m#{self}\e[0m" end
-  end #String class
+    def bg_red
+      "\e[41m#{self}\e[0m"
+    end
+
+    def bg_green
+      "\e[42m#{self}\e[0m"
+    end
+
+    def bg_blue
+      "\e[44m#{self}\e[0m"
+    end
+
+    def bg_magenta
+      "\e[45m#{self}\e[0m"
+    end
+
+    def bg_cyan
+      "\e[46m#{self}\e[0m"
+    end
+
+    def bg_white
+      "\e[47m#{self}\e[0m"
+    end
+  end
 
   class Game
-    attr_reader :player_guess
+
     player_guess = []
     def initialize(string_colors)
       temp_color_choice = []
-      temp_color_choice[0] = "  ".bg_red
-      temp_color_choice[1] = "  ".bg_green
-      temp_color_choice[2] = "  ".bg_blue
-      temp_color_choice[3] = "  ".bg_magenta
-      temp_color_choice[4] = "  ".bg_cyan
-      temp_color_choice[5] = "  ".bg_white
+      temp_color_choice[0] = '  '.bg_red
+      temp_color_choice[1] = '  '.bg_green
+      temp_color_choice[2] = '  '.bg_blue
+      temp_color_choice[3] = '  '.bg_magenta
+      temp_color_choice[4] = '  '.bg_cyan
+      temp_color_choice[5] = '  '.bg_white
       all_color_choices = temp_color_choice
 
       computer_cipher_sequence = []
-      4.times { |i| computer_cipher_sequence.push(temp_color_choice.delete_at(rand(temp_color_choice.length))) }
-    end #end initialize
+      4.times { computer_cipher_sequence.push(temp_color_choice.delete_at(rand(temp_color_choice.length))) }
+    end
 
     def play
       loop do
-        #player selects their choices
+        # player selects their choices
         player_guess = []
-        player_guess_sequence()
+        player_guess_sequence
 
         if player_has_won?
           puts "#{player.name} won! Way to go!"
           puts player_guess
           return
-        elsif
-          turn == 0
-          "Out of turns! You lost."
+        elsif turns.zero?
+          puts 'Out of turns! You lost.'
           return
         else
-          turns - 1
+          turns -= 1
           puts "#{turns} turn(s) left"
-        end # if statement
-      end # loop do
+        end
+      end
 
-        feedback(player_guess)
-    end #end play
+      feedback(player_guess)
+    end
 
     def feedback(player_guess)
       displayed_feedback = []
-      #for each color in computer_cipher_sequence
+      # for each color in computer_cipher_sequence
       computer_cipher_sequence.each_with_index do |color, index|
         player_guess.each do |guess|
 
           if guess == color
-            displayed_feedback.push("  ".bg_red)
+            displayed_feedback.push('  '.bg_red)
           elsif computer_cipher_sequence.any? { |x| x == guess } && guess != color
-            displayed_feedback.push("  ".bg_white)
+            displayed_feedback.push('  '.bg_white)
           else
-            displayed_feedback.push("  ")
-          end #end if statement
-        end #end player_guess.each
-      end #end each computer_cipher_sequence
+            displayed_feedback.push('  ')
+          end
+        end
+      end
 
       puts "#{displayed_feedback.join('')}"
-    end #end feedback
+    end
 
     def player_has_won?
-      computer_cipher_sequence == player_guess ? true : false
-    end #end player_has_won?
+      computer_cipher_sequence == player_guess
+    end
 
     def player_guess_sequence()
       puts "#{player.name} pick 4 choices one at a time by typing from the selections below."
@@ -85,57 +101,55 @@ module MasterMind
         if COLOR_CHOICES.any? == guess
           player_guess.push(guess)
         else
-          puts "Not one of the choices, try again"
-        end #if statement
+          puts 'Not one of the choices, try again'
+        end
 
 
         if player_guess.length == 4
           puts "Your guesses #{player_guess.join('')}"
           return
-        end #if Statement
-      end #end loop
+        end
+      end
 
       player_guess_text_to_color(player_guess)
-    end #player_guess_sequence
+    end
 
     def player_guess_text_to_color(guesses)
       guesses.map! do |text_color|
         case text_color
-        when "red"
-          text_color = "  ".bg_red
-        when "green"
-          text_color = "  ".bg_green
-        when "blue"
-          text_color = "  ".bg_blue
-        when "magenta"
-          text_color = "  ".bg_magenta
-        when "cyan"
-          text_color = "  ".bg_cyan
-        when "white"
-          text_color = "  ".bg_white
-        end #end case
-      end #map! do
-    end #end player_guess_text_to_color
+        when 'red'
+          guesses[text_color] = '  '.bg_red
+        when 'green'
+          guesses[text_color] = '  '.bg_green
+        when 'blue'
+          guesses[text_color] = '  '.bg_blue
+        when 'magenta'
+          guesses[text_color] = '  '.bg_magenta
+        when 'cyan'
+          guesses[text_color] = '  '.bg_cyan
+        when 'white'
+          guesses[text_color] = '  '.bg_white
+        end
+      end
+    end
 
-  end #Game Class
+  end
 
   class Player
     attr_reader :name
 
     def initialize (name)
       @name = name
-    end #initialize
-
-  end #Player Class
+    end
+  end
 
   class ComputerPlayer
-  end #ComputerPlayer
-
-end #MasterMind Module
+  end
+end
 
 include MasterMind
 
-puts "Welcome to Master Mind"
-puts "What is your name?"
+puts 'Welcome to Master Mind'
+puts 'What is your name?'
 instance = String.new
 Game.new(instance).play
